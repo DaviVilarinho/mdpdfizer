24d60c7690fcbc40533b39d48c93128e4b54b933
vilarinux <davi.r.vilarinho@gmail.com>
2021-06-13 18:05:08 -0300

    converter created

--- site/Converting.php
+++ site/Converting.php
@@ -3,12 +3,18 @@
     # class "FileStream"
     ## stores file metadata needed
     class FileStream {
+      protected $DEFAULT_FILE_PATH = "/tmp/";
       public $fileName;
       public $fileType;
     
       public function __construct(String $name, String $type) {
         $this->fileName = $name;
-        $this->fileName = $type;
+        $this->fileType = $type;
+      }
+
+      public function writeFile(String $content) {
+        shell_exec("echo \"$this->fileName.$this->fileType\" > $this->DEFAULT_FILE_PATH$this->fileName.$this->fileType");
+        return 1;
       }
     }
     
@@ -22,9 +28,16 @@
     
       # todo: convert()
     }
-    
+
     # generating unique id for user output file
-    $uniqueID = uniqid("result", false);
+    $uniqueID = uniqid("user-", false);
+    ## Generating input file
+    $inputFile = new FileStream(
+      $mdSource,
+      $mdType
+    );
+
+    
   ?>
 
   <head>
--- site/index.php
+++ site/index.php
@@ -3,8 +3,14 @@
     <title>mdpdfizer</title>
   </head>
   <form action="Converting.php" method="post">
-    Source: <br> <textarea type="text" name="mdsource" cols="80" rows="30"></textarea>
+    Source: <br> <textarea type="text" name="mdSource" cols="80" rows="30"></textarea>
     <br>
+    Input Filetype:
+    <ul>
+      <li><input type="radio" name="mdType" value="gfm"> Markdown</li>
+      <li><input type="radio" name="mdType" value="mediawiki"> Mediawiki</li>
+      <li><input type="radio" name="mdType" value="html"> html</li>
+    </ul>
     <input type="submit">
   </form>
 </html>
