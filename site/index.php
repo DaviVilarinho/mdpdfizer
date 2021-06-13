2c21711eb080ffd794c3394441377d30e1c1c112
vilarinux <davi.r.vilarinho@gmail.com>
2021-06-13 16:10:59 -0300

    Created class FileStream

--- site/index.php
+++ site/index.php
@@ -8,9 +8,38 @@
     <input type="submit">
   </form>
 
-  <?php echo $_GET["mdsource"];?>
   <?php 
-    $output=shell_exec("ls -la");
-    printf("%s", $output);
+    class FileStream {
+      protected $fileName;
+      protected $fileType;
+
+      function __construct(String $name, String $type) {
+        $this->setName($name);
+        $this->setType($type);
+      }
+
+      public function setName(String $file){
+        $this->fileName = $file;
+      }
+
+      public function setType(String $type){
+        $this->fileType = $type;
+      }
+
+      public function getName(){
+        return $this->fileName;
+      }
+
+      public function getType(){
+        return $this->fileType;
+      }
+    }
+
+    # generating unique id
+    $uniqueID = uniqid("result", false);
+    # save file
+    shell_exec("echo \"$mdsource\" > $uniqueID.$inputType");
+    $fileOut=shell_exec("pandoc ");
+
   ?>
 </html>
