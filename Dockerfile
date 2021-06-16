3bef851bc185287e8740f8c5816ffb6d91af5548
vilarinux <davi.r.vilarinho@gmail.com>
2021-06-16 15:09:24 -0300

    cURL working, not parsing

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# Dockerfile for php buster + pandoc image
-FROM php:apache-buster
-
-RUN apt update 
-RUN apt install -y pandoc texlive-full
--- site/Converting.php
+++ site/Converting.php
@@ -1,33 +1,33 @@
 <?php
-  function convertDoc($url, $data = false)
+  function convertDoc($url, $data)
   {
+    $query = http_build_query($data, '', '&');
     $curl = curl_init();
-
-    curl_setopt($curl, CURLOPT_POST, 1);
-    if ($data)
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-
     curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_PORT, 5000);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
+    curl_setopt($curl, CURLOPT_HEADER, false);
 
-
+ 
     $response = curl_exec($curl);
-
     curl_close($curl);
-
     return $response;
   }
+  $mdSource= $_POST['mdSource'];
+  $mdType= $_POST['mdType'];
+  $outType= $_POST['outType'];
 
-  $API_URL    = "http://mdpdfizer_api_1";
-  $jsonResult = json_decode(
-    convertDoc($API_URL,
-      array(
-        "mdSource"=>$_POST['mdSource'],
-        "mdType"=>$_POST['mdType'],
-        "outType"=>$_POST['outType']
-    ))
-  );
-  echo $jsonResult;
+  $API_URL    = "http://192.168.15.114";
+  $response   = convertDoc($API_URL,
+    array(
+      "mdSource"=>$mdSource,
+      "mdType"=>$mdType,
+      "outType"=>$outType
+    ));
+
+  $json = json_decode($response);
+  echo $json[0];
 #  header("Location: " . $response->);
 #  die();
 ?>
