ef1e885974829104ce85aff1e15fa448cc5f5fef
vilarinux <davi.r.vilarinho@gmail.com>
2021-06-16 15:07:08 -0300

    api ready

--- api/converter-api.py
+++ api/converter-api.py
@@ -3,8 +3,9 @@ from flask import Flask, json, jsonify, url_for, request
 from markupsafe import escape
 from uuid import uuid4
 
-INPUT_DIR   = "./in/"
-OUTPUT_DIR  = "./out/"
+pypandoc.ensure_pandoc_installed()
+
+OUTPUT_DIR  = "/api/out/"
 
 DEFAULT_METHOD = 'POST'
 DEFAULT_METHODS=[DEFAULT_METHOD]
@@ -12,25 +13,20 @@ converter = Flask(__name__)
 
 @converter.route("/", methods=DEFAULT_METHODS)
 def convertDoc():
-    error = None
-    mdSource    = request.args.get('mdSource', '')
-    mdType      = request.args.get('mdType', '')
-    outType     = request.args.get('outType')
+    mdSource    = request.form.get('mdSource')
+    mdType      = request.form.get('mdType')
+    outType     = request.form.get('outType')
 
     UUID = str(uuid4())
     outFilepath = OUTPUT_DIR + UUID + ".pdf"
-    output = pypandoc.convert_text(mdSource, format=mdType, to=outType, outputfile=outFilepath)
+    pypandoc.convert_text(source=mdSource, format=mdType, to=outType, encoding='unicode', outputfile=outFilepath)
 
-    if (output):
-        return jsonify({
-            "uuid": UUID,
-            "mdType": mdType,
-            "outType": outType,
-            "outputPath": outFilepath,
-            "mdSource": mdSource,
-        })
-    else:
-        return -1
+    return jsonify({
+        "uuid": UUID,
+        "mdType": mdType,
+        "outType": outType,
+        "outputPath": outFilepath,
+    })
 
 if __name__ == "__main__":
-    converter.run()
+    converter.run(host="0.0.0.0")
